dea4e59b98cdc7689d9131e258a20bbbad6630e6
LAPTOP-BITE348R\Anthony <pattisoa@sheridancollege.ca>
2025-02-16 17:05:49 -0500

    The train follows the track and changes direction based on the rails

--- Assets/Scripts/Coding gym 6/trainpathfinding.cs
+++ Assets/Scripts/Coding gym 6/trainpathfinding.cs
@@ -7,43 +7,86 @@ using static UnityEditor.PlayerSettings;
 public class trainpathfinding : MonoBehaviour
 {
     public float speed = 5;
+    int i = 0;
     public GameObject train;
     public List<GameObject> trainSpots;
     // Start is called before the first frame update
     void Start()
     {
 
-     
+
 
     }
     // Update is called once per frame
     void Update()
     {
         Vector2 trainPosition = train.transform.position;
-        for (int i = 0; i < trainSpots.Count; i++)
+
+        Vector2 destination = trainSpots[i].transform.position;
+
+        Debug.Log(i);
+
+        if (trainPosition.x > destination.x)
         {
-            Vector2 destination = trainSpots[i].transform.position;
-            
-                Debug.Log(i);
-                if (trainPosition.x > destination.x)
-                {
-                    trainPosition.x = trainPosition.x - speed * Time.deltaTime;
-                }
-                if (trainPosition.x < destination.x)
-                {
-                    trainPosition.x = trainPosition.x + speed * Time.deltaTime;
-                }
-                if (trainPosition.y > destination.y)
-                {
-                    trainPosition.y = trainPosition.y - speed * Time.deltaTime;
-                }
-                if (trainPosition.y < destination.y)
-                {
-                    trainPosition.y = trainPosition.y + speed * Time.deltaTime;
-                }
-            
-            train.transform.position = trainPosition;
-            
-            }
+            trainPosition.x -= speed * Time.deltaTime;
+        }
+        if (trainPosition.x < destination.x)
+        {
+            trainPosition.x += speed * Time.deltaTime;
+        }
+        if (trainPosition.y > destination.y)
+        {
+            trainPosition.y -= speed * Time.deltaTime;
+        }
+        if (trainPosition.y < destination.y)
+        {
+            trainPosition.y += speed * Time.deltaTime;
+        }
+
+        train.transform.position = trainPosition;
+
+        if (trainPosition.x <= destination.x + .10f && i == 0)
+        {
+            i = 1;
+            destination = trainSpots[i].transform.position;
+        }
+
+        if (trainPosition.y < destination.y +.3f && trainPosition.y > destination.y && i == 1)
+        {
+            i = 2;
+            destination = trainSpots[i].transform.position;
+        }
+
+        if (trainPosition.x > destination.x && trainPosition.x <= destination.x + .15f && i == 2)
+        {
+            i = 3;
+            destination = trainSpots[i].transform.position;
+        }
+        if (trainPosition.y > destination.y && trainPosition.y <= destination.y + .15f && i == 3)
+        {
+            i = 4;
+            destination = trainSpots[i].transform.position;
+        }
+        if (trainPosition.x > destination.x && trainPosition.x <= destination.x + .15f && i == 4)
+        {
+            i = 5;
+            destination = trainSpots[i].transform.position;
+        }
+        if (trainPosition.y < destination.y + .10f && trainPosition.y > destination.y - .30f && i == 5)
+        {
+            i = 6;
+            destination = trainSpots[i].transform.position;
+        }
+        if (trainPosition.x > destination.x && trainPosition.x < destination.x + .10f && i == 6)
+        {
+            i = 7;
+            destination = trainSpots[i].transform.position;
+        }
+        if (trainPosition.y > destination.y && trainPosition.y <= destination.y + .15f && i == 7)
+        {
+            i = 0;
+            destination = trainSpots[i].transform.position;
+        }
+
     }
 }
