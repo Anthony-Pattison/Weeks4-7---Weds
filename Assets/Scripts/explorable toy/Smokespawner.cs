273a94fc396010a547ca5dce56914e0a8b8e2634
LAPTOP-BITE348R\Anthony <pattisoa@sheridancollege.ca>
2025-02-17 20:49:05 -0500

    The smoke now spawns as a prefabe then gets destroyed after 2 seconds. Then respawns again

--- Assets/Scripts/explorable toy/Smokespawner.cs
+++ Assets/Scripts/explorable toy/Smokespawner.cs
@@ -4,26 +4,38 @@ using UnityEngine;
 
 public class Smokespawner : MonoBehaviour
 {
-
+    
+    int i = 0;
+    int smokeTransparency = 255;
+    public SpriteRenderer SomokeSpriteRenderer;
     public GameObject TrainSmokeprefab;
+    public List<GameObject> SmokeList;
+    GameObject newSomke;
     // Start is called before the first frame update
     void Start()
     {
-
+        //SomokeSpriteRenderer.color = new Color(97, 97, 97, smokeTransparency);
     }
     // Update is called once per frame
     void Update()
     {
         
+        for (; SmokeList.Count < 5; i++) {
             Vector2 pos = transform.position;
-        pos.y += 2;
-        pos.x -= 1;
-            if (Input.GetMouseButtonDown(0))
-            {
-           
-                Instantiate(TrainSmokeprefab, pos, Quaternion.identity);
-            }
-       
+            pos.y += 2;
+            pos.x -= 1;
 
-    }
+            
+            newSomke = Instantiate(TrainSmokeprefab, pos, Quaternion.identity);
+            SmokeList.Add(newSomke);
+            Destroy(newSomke, 2);
+
+        }
+        
+        smokeTransparency--;
+        if (smokeTransparency == 0) {
+            SmokeList.Clear();
+            smokeTransparency = 255;
+        }   
+        }
 }
