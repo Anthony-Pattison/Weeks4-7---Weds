6e487a381cfbf2f4c564d0022ce98d1b404322d2
LAPTOP-BITE348R\Anthony <pattisoa@sheridancollege.ca>
2025-02-17 23:02:24 -0500

    The train follows the tracks, smoke follows the train in the right spot. UI changes the speed of the train and the smoke turns off and on the smoke

--- Assets/Scripts/Coding gym 6/trainpathfinding.cs
+++ Assets/Scripts/Coding gym 6/trainpathfinding.cs
@@ -1,21 +1,20 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Apple;
 using static UnityEditor.PlayerSettings;
 
 public class trainpathfinding : MonoBehaviour
 {
-    public float speed = 5;
+    float speed;
     int i = 0;
     public GameObject train;
     public List<GameObject> trainSpots;
     // Start is called before the first frame update
     void Start()
     {
-
-
-
     }
     // Update is called once per frame
     void Update()
@@ -25,7 +24,6 @@ public class trainpathfinding : MonoBehaviour
         Vector2 destination = trainSpots[i].transform.position;
 
        
-
         if (trainPosition.x > destination.x)
         {
             trainPosition.x -= speed * Time.deltaTime;
@@ -47,46 +45,58 @@ public class trainpathfinding : MonoBehaviour
 
         if (trainPosition.x <= destination.x + .10f && i == 0)
         {
+            train.transform.eulerAngles = new Vector3(0, 0, 90);
             i = 1;
             destination = trainSpots[i].transform.position;
         }
 
         if (trainPosition.y < destination.y +.3f && trainPosition.y > destination.y && i == 1)
         {
+            train.transform.eulerAngles = new Vector3(0, 0, 0);
             i = 2;
             destination = trainSpots[i].transform.position;
         }
 
         if (trainPosition.x > destination.x && trainPosition.x <= destination.x + .15f && i == 2)
         {
+            train.transform.eulerAngles = new Vector3(0, 0, -90);
             i = 3;
             destination = trainSpots[i].transform.position;
         }
         if (trainPosition.y > destination.y && trainPosition.y <= destination.y + .15f && i == 3)
-        {
+        {   
+            train.transform.eulerAngles = new Vector3(0, 0, -180);
             i = 4;
             destination = trainSpots[i].transform.position;
         }
         if (trainPosition.x > destination.x && trainPosition.x <= destination.x + .15f && i == 4)
         {
+            train.transform.eulerAngles = new Vector3(0, 0, -270);
             i = 5;
             destination = trainSpots[i].transform.position;
         }
         if (trainPosition.y < destination.y + .10f && trainPosition.y > destination.y - .30f && i == 5)
         {
+            train.transform.eulerAngles = new Vector3(0, 0, 0);
             i = 6;
             destination = trainSpots[i].transform.position;
         }
         if (trainPosition.x > destination.x && trainPosition.x < destination.x + .10f && i == 6)
         {
+            train.transform.eulerAngles = new Vector3(0, 0, -90);
             i = 7;
             destination = trainSpots[i].transform.position;
         }
         if (trainPosition.y > destination.y && trainPosition.y <= destination.y + .15f && i == 7)
         {
+            train.transform.eulerAngles = new Vector3(0, 0, 0);
             i = 0;
             destination = trainSpots[i].transform.position;
         }
-
+        
+    }
+    public void ChangeSpeed(float c)
+    {
+        speed = c;
     }
 }
--- Assets/Scripts/explorable toy/Smokespawner.cs
+++ Assets/Scripts/explorable toy/Smokespawner.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Smokespawner : MonoBehaviour
 {
-    
+    bool smokeOn;
     int i = 0;
     int smokeTransparency = 255;
     public SpriteRenderer SomokeSpriteRenderer;
@@ -19,23 +19,35 @@ public class Smokespawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        for (; SmokeList.Count < 5; i++) {
-            Vector2 pos = transform.position;
-            pos.y += 2;
-            pos.x -= 1;
+        if (smokeOn)
+        {
 
-            
-            newSomke = Instantiate(TrainSmokeprefab, pos, Quaternion.identity);
-            SmokeList.Add(newSomke);
-            Destroy(newSomke, 2);
+            for (int i = 0; SmokeList.Count < 5; i++)
+            {
 
+                Vector2 pos = transform.position;
+
+                pos.x -= 1f * (0.08f * i);
+                newSomke = Instantiate(TrainSmokeprefab, pos, Quaternion.identity);
+                Vector3 rotate = transform.eulerAngles;
+                SmokeList.Add(newSomke);
+                newSomke.GetComponent<SpriteRenderer>().color = Color.grey;
+                Destroy(newSomke, 2);
+
+            }
+
+            smokeTransparency--;
+            if (smokeTransparency == 0)
+            {
+                SmokeList.Clear();
+                smokeTransparency = 255;
+            }
         }
-        
-        smokeTransparency--;
-        if (smokeTransparency == 0) {
-            SmokeList.Clear();
-            smokeTransparency = 255;
-        }   
-        }
+    }
+    public void SmokeSwitch(bool c)
+    {
+        smokeOn = c;
+    }
 }
+
+
